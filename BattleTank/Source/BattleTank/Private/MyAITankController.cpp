43d7c3f2e575a87722b8ba5656aac2345c589b41
dnguyen7433 <nguyendieuhuyen2809@gmail.com>
2019-01-27 11:16:17 -0600

    BT_Getting the player's controller for AIController

---
+++ BattleTank/Source/BattleTank/Private/MyAITankController.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyAITankController.h"
+#include "GameFramework/Actor.h"
+#include "Engine/World.h"
+
+void AMyAITankController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto AIControlledTank = GetAIControlledTank();
+	if (!AIControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("AIController not possessing tank!!!"))
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("AIController possessing %s!!!"), *(AIControlledTank ->GetName()))
+	}
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank) {
+		UE_LOG(LogTemp, Warning, TEXT("AIController couldn't find the player's tank!!!"))
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("AIController found: %s!!!"), *(PlayerTank->GetName()))
+	}
+}
+
+ATank * AMyAITankController::GetAIControlledTank() const
+
+{
+	auto AIControlledTank = Cast<ATank>(GetPawn());
+	if (!AIControlledTank) {
+		return nullptr;
+	}
+	else {
+		return AIControlledTank;
+	}
+}
+
+ATank * AMyAITankController::GetPlayerTank() const
+{
+	auto PlayerTank = Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+	if (!PlayerTank) {
+		return nullptr;
+	}
+	else {
+		return  PlayerTank;
+	}
+}
---
+++ BattleTank/Source/BattleTank/Public/MyAITankController.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "Tank.h"
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "MyAITankController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API AMyAITankController : public AAIController
+{
+	GENERATED_BODY()
+private:
+	void BeginPlay() override;
+	
+	ATank* GetAIControlledTank() const;
+	//Post: Possessing tanks that are not possessed by the player
+	ATank* GetPlayerTank() const;
+	//Post: Return the player's controlled tank with the purpose of aiming
+};
