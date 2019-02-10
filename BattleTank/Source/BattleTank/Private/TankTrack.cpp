59be342bedd79481a254dd27b989658399b42940
dnguyen7433 <nguyendieuhuyen2809@gmail.com>
2019-02-10 10:30:38 -0600

    BT_Create the C++ TankTrack to control the left and right track AND Input binding to Left and Right track Throttles

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+
+
+void UTankTrack::SetThrottles(float RelativeSpeed) {
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s throttles at: %f"), *Name, RelativeSpeed)
+
+	// TODO Clamp actual throttle value so players can't change too much
+}
+
